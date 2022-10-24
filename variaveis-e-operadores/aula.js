de57c6405596b0443e763e9052a451810c0f2e14
Carlos <carloskyle17@gmail.com>
2022-10-24 20:40:01 -0300

    Desafio Concluido

--- variaveis-e-operadores/aula.js
+++ variaveis-e-operadores/aula.js
@@ -1,12 +1,17 @@
 
+// PreçoCombustivel = 5,59l
+// ValorMedio = 14km/l
+// DistanciaKm = 20km
+// ValorGasto = ValorMedio / DistanciaKm * PreçoCombustivel
+// Valor gasto será de (ValorMedio / DistanciaKm ) 
 
-let variavel = 10+10;
+
+let preçoCombustivel = 5.60;
+let kmPorLitros = 14;
+let distanciaKm = 20;
+let valorGasto = ( distanciaKm / kmPorLitros * preçoCombustivel);
 
 
 
 
-console.log(variavel)
-
-variavel = 20/2;
-
-console.log(variavel)
+console.log('O valor gasto de combustível será de', valorGasto, 'R$');
